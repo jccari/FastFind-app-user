74e051da230423e0491a7f49e503da021e85ef35
jccari <jhoelccari.23@gmail.com>
2020-05-27 22:15:06 -0500

    define app palette

---
+++ src/constants/Colors.js
@@ -0,0 +1,13 @@
+export default {
+    primary: '#FF6969',
+    secondary: '#515C6F',
+    background: '#F5F6F8',
+    success: '#71e100',
+    danger: '#d62409',
+    warning: '#ff8800',
+    info: '#17a2b8',
+    dark: '#2c2938',
+    white: '#ffffff',
+    light: '#e7e8e9',
+    unchecked: '#c5c5c5',
+};
