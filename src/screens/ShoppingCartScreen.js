f45f6191c5467c3ef7b6365b72cb82d69a7e516d
jccari <jhoelccari.23@gmail.com>
2020-07-30 00:21:19 -0500

    empty cart

--- src/screens/ShoppingCartScreen.js
+++ src/screens/ShoppingCartScreen.js
@@ -1,7 +1,6 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {RFValue} from "react-native-responsive-fontsize";
-import {Divider, ListItem} from 'react-native-elements';
+import {View, Text, StyleSheet, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {Divider, Image} from 'react-native-elements';
 import Colors from '../constants/Colors';
 
 import {AppContext} from '../contexts/AppContext';
@@ -14,10 +13,17 @@ export default function ShoppingCartScreen(){
 
     if(shoppingCart.length === 0){
         return (
-            <View style={styles.screenContainer}>
-                <Text> Tu lista de compras </Text>
-                <Divider style={{backgroundColor:Colors.secondary}}/>
-                <Text> Carrito Vacio </Text>
+            <View style={styles.screenEmptyContainer}>
+                <Image
+                    source={{ uri: "https://firebasestorage.googleapis.com/v0/b/fastfinduserapp.appspot.com/o/cesta.png?alt=media&token=5d2258f7-b59f-4db1-9a2b-d59292f99bac" }}
+                    style={{ width: 200, height: 200, alignSelf: "center", marginBottom:20 }}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+                <Text style={styles.emptyText}> Whoops!</Text>
+                <Text style={[styles.emptyText,{marginHorizontal: 20}]}> Parece que tu carrito de compras está vacio</Text>
+                <TouchableOpacity>
+                    <Text style={[styles.titleText, styles.button,{fontWeight:"bold"}]}> BUSCAR PRODUCTOS </Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -43,6 +49,18 @@ export default function ShoppingCartScreen(){
 }
 
 const styles = StyleSheet.create({
+    screenEmptyContainer: {
+        flex:1 ,
+        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: "center",
+    },
+    emptyText:{
+        color: Colors.primary,
+        fontSize: 30,
+        marginTop: 20,
+        textAlign: "center",
+    },
     screenContainer: {
         flex:1 ,
         flexDirection: "column",
@@ -53,8 +71,16 @@ const styles = StyleSheet.create({
         fontSize: 25,
         color: Colors.secondary,
         paddingVertical: 15,
+        // marginTop: 20,
     },
     listItem: {
         width: wp(90)
+    },
+    button:{
+        backgroundColor: Colors.primary,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 15,
+        marginTop: 55,
     }
 });
